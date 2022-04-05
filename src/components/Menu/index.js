21872d2095d0d27294e9088fc36d9092c5b65c41
Martin-Gab <martingabillet@gmail.com>
2022-04-05 12:21:15 +0200

    install-react-feather-icons & logo-and-burger-menu_Martin

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,9 +2,16 @@
 
 // Local Imports
 import './menu.scss';
+import logo from 'src/assets/images/logo.png';
+
+// Package Imports
+import FeatherIcon from 'feather-icons-react';
 
 const Menu = () => (
-  <h1>Ici le menu</h1>
+  <div className="menu-wrapper">
+    <img src={logo} alt="Le logo de tomates et potirons" className="main-logo" />
+    <FeatherIcon icon="menu" size={36} className="menu-burger" />
+  </div>
 );
 
 export default Menu;
