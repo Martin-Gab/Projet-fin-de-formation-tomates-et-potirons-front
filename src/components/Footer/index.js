a2c0fb7f6ac25002e04e4b14960845f9b912f87a
Martin-Gab <martingabillet@gmail.com>
2022-04-06 11:13:11 +0200

    [wip]Footer_Martin

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,28 @@
 import './footer.scss';
 
 const Footer = () => (
-  <div>Ici le footer</div>
+  <footer>
+    <div className="contact">
+      <h2 className="footer-title">Contact</h2>
+      <p className="contact__infos">Tomates & Potirons</p>
+      <p className="contact__infos">14 rue xxxxxxxxxx</p>
+      <p className="contact__infos">12345 Poitiers</p>
+      <p className="contact__infos">tomates&potirons@gmail.com</p>
+      <p className="contact__infos">06 09 45 34 34</p>
+    </div>
+    <div className="navigation">
+      <h2 className="footer-title">Navigation</h2>
+      <a href="#">Accueil</a>
+      <a href="#">Vente Direct</a>
+      <a href="#">AMAP</a>
+      <a href="#">Nos Partenaires</a>
+      <a href="#">Vos Maraîchers</a>
+      <a href="#">Notre Agriculture</a>
+    </div>
+    <div className="other">
+      <h2>Nos Réseaux</h2>
+    </div>
+  </footer>
 );
 
 export default Footer;
