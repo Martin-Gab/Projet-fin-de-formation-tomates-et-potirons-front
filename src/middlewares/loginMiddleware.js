41abff8671eef0d8d9c5aeea15671e0e27b4b64d
MartinMacbook <martingabillet@gmail.com>
2022-04-08 21:08:31 +0200

    setup-loginMiddleware_Martin

---
+++ src/middlewares/loginMiddleware.js
@@ -0,0 +1,19 @@
+// Middleware to handle the user login
+
+// Packages Imports
+
+// Local Imports
+
+const loginMiddelware = (store) => (next) => (action) => {
+  switch (action.type) {
+    // case value:
+
+    //   break;
+
+    default:
+  }
+
+  next(action);
+};
+
+export default loginMiddelware;
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,14 @@
+// Packages Imports
 import { createStore, applyMiddleware, compose } from 'redux';
 
+// Local Imports
 import reducer from 'src/reducers';
+import loginMiddelware from 'src/middlewares/loginMiddleware';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const enhancers = composeEnhancers(
-  applyMiddleware(),
+  applyMiddleware(loginMiddelware),
 );
 
 const store = createStore(reducer, enhancers);
