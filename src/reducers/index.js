0e0ef676e6ac782f8191ba99bbe645d044d6d6a7
Martin-Gab <martingabillet@gmail.com>
2022-04-08 19:11:41 +0200

    actions-login & reducer-login_Martin

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 
 import membershipReducer from './membership';
 import menuReducer from './menu';
+import loginReducer from './login';
 
 const rootReducer = combineReducers({
   // nom du tiroir: reducer qui gère cette partie du state
   membership: membershipReducer,
   menu: menuReducer,
+  login: loginReducer,
 });
 
 export default rootReducer;
---
+++ src/reducers/login.js
@@ -0,0 +1,18 @@
+// Reducer for login form
+
+export const initialState = {
+  isOpen: true,
+};
+
+const reducer = (state = initialState, action = {}) => {
+  switch (action.type) {
+    // case value:
+
+    //   break;
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
