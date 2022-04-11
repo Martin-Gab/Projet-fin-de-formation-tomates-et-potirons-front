5d34a1223990e95fe9a91ed982421551cb006f0b
Martin-Gab <martingabillet@gmail.com>
2022-04-11 16:16:21 +0200

    profil-page-OK_Martin

--- src/components/MemberProfil/index.js
+++ src/components/MemberProfil/index.js
@@ -3,8 +3,33 @@
 // Local Imports
 import './member-profil.scss';
 
-const MemberProfil = () => (
-  <div>Ici le profil de l'adhérent</div>
-);
+// Packages Imports
+import { useSelector } from 'react-redux';
+
+const MemberProfil = () => {
+  const user = useSelector((state) => state.user.user);
+
+  return (
+    <main>
+      <div className="profil-wrapper">
+        <div className="profil">
+          <h1 className="profil__title">Mon Profil</h1>
+          <div className="profil__underline" />
+          <div className="profil__item">Prénom : {user.firstName}</div>
+          <div className="profil__item">Nom : {user.lastName}</div>
+          <div className="profil__item">Adresse : {user.address}, {user.zipCode} {user.city}</div>
+          <div className="profil__item">Adresse email : {user.email}</div>
+          <div className="profil__item">Numéro de téléphone : {user.phone}</div>
+          <div className="profil__item">
+            Mot de passe : <input type="password" value={user.password} className="profil__password" readOnly />
+          </div>
+          <div className="cta-wrapper">
+            <div className="profil__cta">Modifier mon Profil</div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
 
 export default MemberProfil;
