84aea9bf1bde117de1bf7e9a53f909390b6caacf
Martin-Gab <martingabillet@gmail.com>
2022-04-18 09:28:57 +0200

    content-for-page & install-react-google-maps/api_Martin

--- src/components/VenteDirect/index.js
+++ src/components/VenteDirect/index.js
@@ -4,7 +4,30 @@
 import './vente-direct.scss';
 
 const VenteDirect = () => (
-  <div>Ici la page Vente Direct</div>
+  <main>
+    <div className="vente-direct-wrapper">
+      <div className="vente-direct-map">Ici la google map</div>
+      <div className="vente-direct-content">
+        <h1 className="vente-direct-content__title">Vente Direct</h1>
+        <p className="vente-direct-content__description">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Cum sit officia earum veniam est! Ipsa necessitatibus,
+          soluta sequi quae repudiandae excepturi in dolores fugiat
+          temporibus corrupti repellat pariatur est tempora.
+        </p>
+        <p className="vente-direct-content__location">
+          A la ferme <br />
+          27 route d'Anxaumont <br />
+          86800 Sèvres-Anxaumont
+        </p>
+        <p className="vente-direct-content__hours">
+          Mercredi : 14h-17h30 <br />
+          Vendredi : 14h-17h30 <br />
+          Samedi : 09h-12H30
+        </p>
+      </div>
+    </div>
+  </main>
 );
 
 export default VenteDirect;
