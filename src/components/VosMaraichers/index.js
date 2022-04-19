49251515020e03989bc1390944d7976379ad1217
Martin-Gab <martingabillet@gmail.com>
2022-04-19 14:17:09 +0200

    css-OK & responsive-OK_Martin

--- src/components/VosMaraichers/index.js
+++ src/components/VosMaraichers/index.js
@@ -2,9 +2,47 @@
 
 // Local Imports
 import './vos-maraichers.scss';
+import pictureFrancois from 'src/assets/images/photo-francois.jpeg';
+import pictureThomas from 'src/assets/images/photo-thomas.jpeg';
 
 const VosMaraichers = () => (
-  <div>Ici la page Vos Maraichers</div>
+  <main>
+    <div className="vos-maraichers-wrapper">
+
+      <img src={pictureFrancois} alt="François, un des maraichers" className="vos-maraichers-picture vos-maraichers-picture--first" />
+
+      <div className="vos-maraichers-content vos-maraichers-content--first">
+        <h1 className="vos-maraichers-content__name">François</h1>
+        <p className="vos-maraichers-content__description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Autem beatae id mollitia repellendus asperiores perspiciatis
+          labore commodi quasi non similique nesciunt, repellat eligendi,
+          in tempora fugit molestiae? Odit, fugit vitae?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Autem beatae id mollitia repellendus asperiores perspiciatis
+          labore commodi quasi non similique nesciunt, repellat eligendi,
+          in tempora fugit molestiae? Odit, fugit vitae?
+        </p>
+      </div>
+
+      <img src={pictureThomas} alt="Thomas, un des maraichers" className="vos-maraichers-picture vos-maraichers-picture--second" />
+
+      <div className="vos-maraichers-content vos-maraichers-content--second">
+        <h1 className="vos-maraichers-content__name">Thomas</h1>
+        <p className="vos-maraichers-content__description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Autem beatae id mollitia repellendus asperiores perspiciatis
+          labore commodi quasi non similique nesciunt, repellat eligendi,
+          in tempora fugit molestiae? Odit, fugit vitae?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Autem beatae id mollitia repellendus asperiores perspiciatis
+          labore commodi quasi non similique nesciunt, repellat eligendi,
+          in tempora fugit molestiae? Odit, fugit vitae?
+        </p>
+      </div>
+
+    </div>
+  </main>
 );
 
 export default VosMaraichers;
