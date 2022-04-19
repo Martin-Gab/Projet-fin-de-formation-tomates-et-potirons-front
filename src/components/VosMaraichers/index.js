239473da15bf7dc0b1876d47631ac5651be699e2
Martin-Gab <martingabillet@gmail.com>
2022-04-19 14:23:24 +0200

    scroll-to-top-on-page-load_

--- src/components/VosMaraichers/index.js
+++ src/components/VosMaraichers/index.js
@@ -5,44 +5,64 @@ import './vos-maraichers.scss';
 import pictureFrancois from 'src/assets/images/photo-francois.jpeg';
 import pictureThomas from 'src/assets/images/photo-thomas.jpeg';
 
-const VosMaraichers = () => (
-  <main>
-    <div className="vos-maraichers-wrapper">
+// Packages Imports
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
-      <img src={pictureFrancois} alt="François, un des maraichers" className="vos-maraichers-picture vos-maraichers-picture--first" />
+// Actions Imports
+import { closeBurgerMenu, closeSubMenu } from 'src/actions/menu';
 
-      <div className="vos-maraichers-content vos-maraichers-content--first">
-        <h1 className="vos-maraichers-content__name">François</h1>
-        <p className="vos-maraichers-content__description">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Autem beatae id mollitia repellendus asperiores perspiciatis
-          labore commodi quasi non similique nesciunt, repellat eligendi,
-          in tempora fugit molestiae? Odit, fugit vitae?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Autem beatae id mollitia repellendus asperiores perspiciatis
-          labore commodi quasi non similique nesciunt, repellat eligendi,
-          in tempora fugit molestiae? Odit, fugit vitae?
-        </p>
+const VosMaraichers = () => {
+  const dispatch = useDispatch();
+
+  useEffect(
+    () => {
+      window.scrollTo(0, 0);
+      dispatch(closeBurgerMenu());
+      dispatch(closeSubMenu());
+    },
+    [],
+  );
+
+  return (
+    <main>
+      <div className="vos-maraichers-wrapper">
+
+        <img src={pictureFrancois} alt="François, un des maraichers" className="vos-maraichers-picture vos-maraichers-picture--first" />
+
+        <div className="vos-maraichers-content vos-maraichers-content--first">
+          <h1 className="vos-maraichers-content__name">François</h1>
+          <p className="vos-maraichers-content__description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Autem beatae id mollitia repellendus asperiores perspiciatis
+            labore commodi quasi non similique nesciunt, repellat eligendi,
+            in tempora fugit molestiae? Odit, fugit vitae?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Autem beatae id mollitia repellendus asperiores perspiciatis
+            labore commodi quasi non similique nesciunt, repellat eligendi,
+            in tempora fugit molestiae? Odit, fugit vitae?
+          </p>
+        </div>
+
+        <img src={pictureThomas} alt="Thomas, un des maraichers" className="vos-maraichers-picture vos-maraichers-picture--second" />
+
+        <div className="vos-maraichers-content vos-maraichers-content--second">
+          <h1 className="vos-maraichers-content__name">Thomas</h1>
+          <p className="vos-maraichers-content__description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Autem beatae id mollitia repellendus asperiores perspiciatis
+            labore commodi quasi non similique nesciunt, repellat eligendi,
+            in tempora fugit molestiae? Odit, fugit vitae?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Autem beatae id mollitia repellendus asperiores perspiciatis
+            labore commodi quasi non similique nesciunt, repellat eligendi,
+            in tempora fugit molestiae? Odit, fugit vitae?
+          </p>
+        </div>
+
       </div>
-
-      <img src={pictureThomas} alt="Thomas, un des maraichers" className="vos-maraichers-picture vos-maraichers-picture--second" />
-
-      <div className="vos-maraichers-content vos-maraichers-content--second">
-        <h1 className="vos-maraichers-content__name">Thomas</h1>
-        <p className="vos-maraichers-content__description">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Autem beatae id mollitia repellendus asperiores perspiciatis
-          labore commodi quasi non similique nesciunt, repellat eligendi,
-          in tempora fugit molestiae? Odit, fugit vitae?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Autem beatae id mollitia repellendus asperiores perspiciatis
-          labore commodi quasi non similique nesciunt, repellat eligendi,
-          in tempora fugit molestiae? Odit, fugit vitae?
-        </p>
-      </div>
-
-    </div>
-  </main>
-);
+    </main>
+  );
+};
 
 export default VosMaraichers;
