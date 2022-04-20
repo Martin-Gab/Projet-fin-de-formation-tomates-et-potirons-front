bebc71b72b7a569a9d0a16c4e740f796c13ee7bc
Martin-Gab <martingabillet@gmail.com>
2022-04-20 12:19:05 +0200

    [fix]scroll-top-close-menu-notre-agriculture-page_Martin

--- src/components/NotreAgriculture/index.js
+++ src/components/NotreAgriculture/index.js
@@ -4,32 +4,52 @@
 import './notre-agriculture.scss';
 import pictureAgriculture from 'src/assets/images/notre-agriculture.jpeg';
 
-const NotreAgriculture = () => (
-  <main>
-    <div className="notre-agriculture-wrapper">
+// Packages Imports
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
-      <img src={pictureAgriculture} alt="légumes récoltéeschez tomates et potirons" className="notre-agriculture-picture" />
+// Actions Imports
+import { closeBurgerMenu, closeSubMenu } from 'src/actions/menu';
 
-      <div className="notre-agriculture-content">
-        <h1 className="notre-agriculture-content__title">Notre Agriculture</h1>
-        <p className="notre-agriculture-content__text">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Molestias quia quos quaerat velit enim dicta doloribus neque
-          sequi aspernatur ipsam provident consequuntur maxime,
-          consectetur architecto natus id ipsa magni dolorum. <br />
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Molestias quia quos quaerat velit enim dicta doloribus neque
-          sequi aspernatur ipsam provident consequuntur maxime,
-          consectetur architecto natus id ipsa magni dolorum. <br />
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Molestias quia quos quaerat velit enim dicta doloribus neque
-          sequi aspernatur ipsam provident consequuntur maxime,
-          consectetur architecto natus id ipsa magni dolorum. <br />
-        </p>
+const NotreAgriculture = () => {
+  const dispatch = useDispatch();
+
+  useEffect(
+    () => {
+      window.scrollTo(0, 0);
+      dispatch(closeBurgerMenu());
+      dispatch(closeSubMenu());
+    },
+    [],
+  );
+
+  return (
+    <main>
+      <div className="notre-agriculture-wrapper">
+
+        <img src={pictureAgriculture} alt="légumes récoltéeschez tomates et potirons" className="notre-agriculture-picture" />
+
+        <div className="notre-agriculture-content">
+          <h1 className="notre-agriculture-content__title">Notre Agriculture</h1>
+          <p className="notre-agriculture-content__text">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Molestias quia quos quaerat velit enim dicta doloribus neque
+            sequi aspernatur ipsam provident consequuntur maxime,
+            consectetur architecto natus id ipsa magni dolorum. <br />
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Molestias quia quos quaerat velit enim dicta doloribus neque
+            sequi aspernatur ipsam provident consequuntur maxime,
+            consectetur architecto natus id ipsa magni dolorum. <br />
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Molestias quia quos quaerat velit enim dicta doloribus neque
+            sequi aspernatur ipsam provident consequuntur maxime,
+            consectetur architecto natus id ipsa magni dolorum. <br />
+          </p>
+        </div>
+
       </div>
-
-    </div>
-  </main>
-);
+    </main>
+  );
+};
 
 export default NotreAgriculture;
